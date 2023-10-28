ab0fd4d8728544cbce2cce24e1409949e09b9285
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-10-28 10:19:58 +0700

    up: sidebar icon

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -12,7 +12,7 @@
         </li>
         <li>
           <a href="{{ route('dashboard.category.index') }}" class="pl-6 pr-4 py-3 block text-sm font-medium text-gray-700 transition-all duration-150 ease-linear hover:text-violet-500 dark:text-gray-300 dark:active:text-white dark:hover:text-white">
-            <i data-feather="category"></i>
+            <i data-feather="list"></i>
             <span data-key="t-category"> Category</span>
           </a>
         </li>
