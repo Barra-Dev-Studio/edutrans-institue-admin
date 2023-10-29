d15eb9a98b4fa6ddcc7a63b2e1a9d03817903278
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-10-29 20:47:04 +0700

    up: vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         @if(env('APP_ENV') === 'local')
         @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/icons.css'])
         @else
-        <link rel="stylesheet" href="{{ asset('build/assets/app-605fc313.css') }}">
+        <link rel="stylesheet" href="{{ asset('build/assets/app-46f428fb.css') }}">
         <link rel="stylesheet" href="{{ asset('build/assets/icons-adf300ab.css') }}">
         <script src="{{ asset('build/assets/app-02317797.js') }}"></script>
         @endif
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,25 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>{{ config("app.name", "Laravel") }}</title>
+        <title>Edutrans Institute</title>
+        <meta name="title" content="Edutrans Institute" />
+        <meta name="description"
+            content="EDUTRANS.ID adalah lembaga training yang bertujuan untuk mengoptimasi proses pendidikan yang bermutu untuk menciptakan transformasi positif dalam kehidupan individu, organisasi, dan lembaga. Pendiri EDUTRANS.ID Dr. Hendi Pratama, percaya bahwa optimalisasi pendidikan merupakan kunci peningkatan produktivitas dan kesejahteraan." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://edutransinstitute.com" />
+        <meta property="og:title" content="Edutrans Institute" />
+        <meta property="og:description"
+            content="EDUTRANS.ID adalah lembaga training yang bertujuan untuk mengoptimasi proses pendidikan yang bermutu untuk menciptakan transformasi positif dalam kehidupan individu, organisasi, dan lembaga. Pendiri EDUTRANS.ID Dr. Hendi Pratama, percaya bahwa optimalisasi pendidikan merupakan kunci peningkatan produktivitas dan kesejahteraan." />
+        <meta property="og:image" content="https://edutransinstitute.com/assets/images/og.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://edutransinstitute.com" />
+        <meta property="twitter:title" content="Edutrans Institute" />
+        <meta property="twitter:description"
+            content="EDUTRANS.ID adalah lembaga training yang bertujuan untuk mengoptimasi proses pendidikan yang bermutu untuk menciptakan transformasi positif dalam kehidupan individu, organisasi, dan lembaga. Pendiri EDUTRANS.ID Dr. Hendi Pratama, percaya bahwa optimalisasi pendidikan merupakan kunci peningkatan produktivitas dan kesejahteraan." />
+        <meta property="twitter:image" content="https://edutransinstitute.com/assets/images/og.png" />
+        
         @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/icons.css'])
     </head>
     <body data-mode="light" data-sidebar-size="lg">
