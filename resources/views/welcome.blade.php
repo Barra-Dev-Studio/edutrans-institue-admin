23bb2fb2350dc3270a68c7096599325dfeb88f77
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-10-29 20:31:02 +0700

    up: metatags and force https

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
-    <div class="container-fluid flex justify-center h-screen items-center flex-col">
-        <h1 class="text-4xl text-slate-600">Barra Dev Solution</h1>
-        <p class="mt-2 text-slate-400">Solution for your business and digitalization</p>
-    </div>
+  <div class="container-fluid flex justify-center h-screen items-center flex-col">
+    <h1 class="text-4xl text-slate-600">Edutrans Institute Solution</h1>
+    <p class="mt-2 text-slate-400">Solution for your business and digitalization</p>
+  </div>
 </x-guest-layout>
