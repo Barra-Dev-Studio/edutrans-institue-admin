b614daec9506c665515f2e878ec303f81d9e1a4c
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-11-06 16:08:56 +0700

    up: prose problem

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
     <div class="md:p-16">
-        <div class="grid grid-cols-1 md:grid-cols-2 items-start justify-between px-6 md:px-8 mb-16">
+        <div class="grid grid-cols-1 md:grid-cols-2 items-start justify-between px-6 md:px-8 mb-16 w-full">
             <div>
                 <h1 class="mt-0">Jelajahi kursus bersama Edutrans Institute</h1>
             </div>
-            <div class="md:text-right prose pt-3">
-                <p>Jelajahi berbagai kursus di Edutrans Institute dan mulai raih mimpi dan karir dengan aksi bersama Edutrans</p>
+            <div class="md:text-right pt-3">
+                <p class="text-lg text-slate-500">Jelajahi berbagai kursus di Edutrans Institute dan mulai raih mimpi dan karir dengan aksi bersama Edutrans</p>
             </div>
         </div>
         <div class="px-6 md:px-8">
