9b89267685f9aacbb5f20b870dc12591503b52e0
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-12-12 22:51:48 +0700

    up: s3 amazon

--- app/Livewire/Plugin/PlyrLivewire.php
+++ app/Livewire/Plugin/PlyrLivewire.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Plugin;
 
+use Aws\S3\S3Client;
 use Livewire\Component;
 
 class PlyrLivewire extends Component
@@ -16,6 +17,30 @@ class PlyrLivewire extends Component
         if (str_contains($this->embedId, 'youtube.com')) {
             $this->source = 'youtube';
         }
+
+        if (!str_contains($this->embedId, 'youtube.com')) {
+            $this->processThePresignedUrl();
+        }
+    }
+
+    private function processThePresignedUrl(): void
+    {
+        $s3Client = new S3Client([
+            'version' => 'latest',
+            'region' => env('AWS_DEFAULT_REGION'),
+            'credentials' => [
+                'key' => env('AWS_ACCESS_KEY_ID'),
+                'secret' => env('AWS_SECRET_ACCESS_KEY')
+            ]
+        ]);
+
+        $command = $s3Client->getCommand('GetObject', [
+            'Bucket' => env('AWS_BUCKET'),
+            'Key' => $this->embedId
+        ]);
+
+        $request = $s3Client->createPresignedRequest($command, '+15 minutes');
+        $this->embedId = (string) $request->getUri();
     }
 
     public function render()
