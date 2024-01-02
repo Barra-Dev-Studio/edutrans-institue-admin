80f8ce7a92fe4c3dfd936ef976b8e2a8492ec24c
Khoerul Umam <khoerul.umam@gotoko.co.id>
2024-01-02 10:03:35 +0700

    up: upsert va

--- app/Http/Controllers/UserVirtualAccountController.php
+++ app/Http/Controllers/UserVirtualAccountController.php
@@ -2,24 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use App\Models\UserVirtualAccount;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use DB;
 
 class UserVirtualAccountController extends Controller
 {
     public function callbackUpsertVA(Request $request)
     {
+        DB::beginTransaction();
         try {
             $transactionId = $request->external_id;
             $response = json_encode($request->all());
-            UserVirtualAccount::create([
-                'transaction_id' => $transactionId,
-                'response' => $response
-            ]);
+
+            $checkIfExists = UserVirtualAccount::where('transaction_id', $transactionId)->first();
+            if ($checkIfExists !== null) {
+                UserVirtualAccount::where('id', $checkIfExists->id)->update([
+                    'transaction_id' => $transactionId,
+                    'response' => $response
+                ]);
+                Transaction::where('id', $checkIfExists->transaction_id)->update([
+                    'status' => $request->status
+                ]);
+            } else {
+                UserVirtualAccount::create([
+                    'transaction_id' => $transactionId,
+                    'response' => $response
+                ]);
+            }
+            DB::commit();
             return response([], 200);
         } catch (\Exception $e) {
             Log::error($e);
+            DB::rollBack();
             return response($e->getMessage(), 500);
         }
     }
