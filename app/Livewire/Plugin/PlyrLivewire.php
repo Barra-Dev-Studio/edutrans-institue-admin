84547bdaffd82fc734ac7866699f45fb8ce47e10
Khoerul Umam <khoerul.umam@gotoko.co.id>
2024-01-03 10:39:06 +0700

    up: replace with cloudfront

--- app/Livewire/Plugin/PlyrLivewire.php
+++ app/Livewire/Plugin/PlyrLivewire.php
@@ -26,7 +26,8 @@ class PlyrLivewire extends Component
 
     private function processThePresignedUrl(): void
     {
-        $this->embedId = Storage::disk('s3')->temporaryUrl($this->embedId, Carbon::now()->addMinutes(120));
+        $embedId = Storage::disk('s3')->temporaryUrl($this->embedId, Carbon::now()->addMinutes(120));
+        $this->embedId = str_replace('edutransinstitute.s3.ap-southeast-1.amazonaws.com', 'dbtu2xul1ioz8.cloudfront.net', $embedId);
     }
 
     public function render()
