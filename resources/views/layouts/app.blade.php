57f89bceb514009a2a8e4a15cbdb6066d0ce9415
Khoerul Umam <khoerul.umam@gotoko.co.id>
2024-01-12 22:36:45 +0700

    up: css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
   @if(env('APP_ENV') === 'local')
   @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/icons.css'])
   @else
-  <link rel="stylesheet" href="{{ asset('build/assets/app-206b98f3.css') }}">
+  <link rel="stylesheet" href="{{ asset('build/assets/app-79eb1493.css') }}">
   <link rel="stylesheet" href="{{ asset('build/assets/icons-adf300ab.css') }}">
   <script src="{{ asset('build/assets/app-02317797.js') }}"></script>
   @endif
